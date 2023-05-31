26ff64f087bc8ab4276f99edb94b07f9646810d2
Om Ambarkar <heyom7@gmail.com>
2023-05-31 16:51:53 +0530

    1June2023

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -27,7 +27,9 @@ class LoginPage extends StatelessWidget {
           ),
           const Align(
             alignment: Alignment.center,
-            child: LoginPageStful(),
+            child: SingleChildScrollView(
+              child: LoginPageStful(),
+            ),
           ),
         ],
       ),
@@ -49,138 +51,152 @@ class _LoginPageStfulState extends State<LoginPageStful> {
         TextEditingController();
     final TextEditingController passwordTextController =
         TextEditingController();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.asset(
-          "assets/logo/smartcomm_logo_white.png",
-          width: getDeviceWidth(context) * 0.7,
-          height: getDeviceWidth(context) * 0.2,
-        ),
-        SizedBox(
-          height: getDeviceHeight(context) * 0.12,
-        ),
-
-        Container(
-          height: getDeviceHeight(context) * 0.5,
-          width: getDeviceWidth(context) * 0.95,
-          decoration: BoxDecoration(
-            color: BasicValues.basicBlue2.withOpacity(0.85),
-            borderRadius: BorderRadius.circular(10),
+    return Container(
+      height: getDeviceHeight(context),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            BasicValues.smartCommLogoWhite,
+            width: getDeviceWidth(context) * 0.7,
+            height: getDeviceWidth(context) * 0.2,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const myText(
-                padding: EdgeInsets.only(top: 20),
-                text: "Welcome",
-                color: Colors.white,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Divider(
-                  color: Colors.white.withOpacity(0.8),
-                  height: 2,
-                  thickness: 2,
+          SizedBox(
+            height: getDeviceHeight(context) * 0.12,
+          ),
+
+          Container(
+            height: 400,
+            width: getDeviceWidth(context) * 0.95,
+            decoration: BoxDecoration(
+              color: BasicValues.basicBlue2.withOpacity(0.85),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const myText(
+                  padding: EdgeInsets.only(top: 20),
+                  text: "Welcome",
+                  color: Colors.white,
                 ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              input_cards(
-                hinttext: "Username",
-                imgpath: "assets/login/account_profile.png",
-                textEditingController: usernameTextController,
-              ),
-              input_cards(
-                hinttext: "Password",
-                ispassword: true,
-                imgpath: 'assets/login/icon_eye.png',
-                textEditingController: passwordTextController,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ForgotPassword()));
-                      },
-                      child: const myText(
-                        text: "Forgot Password?",
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Divider(
+                    color: Colors.white.withOpacity(0.8),
+                    height: 2,
+                    thickness: 2,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                input_cards(
+                  hinttext: "Username",
+                  imgpath: "assets/login/account_profile.png",
+                  textEditingController: usernameTextController,
+                ),
+                input_cards(
+                  hinttext: "Password",
+                  ispassword: true,
+                  imgpath: 'assets/login/icon_eye.png',
+                  textEditingController: passwordTextController,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ForgotPassword()));
+                        },
+                        child: const myText(
+                          text: "Forgot Password?",
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ischecking_req
+                      ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : myButton(
+                          text: "LOGIN",
+                          textSize: 18,
+                          backgroundColor: BasicValues.gray2.withOpacity(0.6),
+                          borderRadius: 5,
+                          onPress: () {
+                            ischecking_req = true;
+                            setState(() {});
+                            // checkValidityUser("Super_User", "Super@123456")
+                            checkValidityUser(usernameTextController.text,
+                                    passwordTextController.text)
+                                .then((value) {
+                              if (value == true) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const HomePage()));
+                              } else {
+                                ischecking_req = false;
+                                setState(() {});
+                                toastMessagePopup(
+                                    context, "Invalid Credentials");
+                              }
+                            });
+                          },
+                        ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    toastMessagePopup(context,
+                        "This feature will be available in the future update");
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "New to Smartcomm? Sign up Now",
+                      style: TextStyle(
                         color: Colors.white,
                         fontSize: 18,
+                        decoration: TextDecoration.underline,
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: myButton(
-                  text: "LOGIN",
-                  textSize: 18,
-                  backgroundColor: BasicValues.gray2.withOpacity(0.6),
-                  borderRadius: 5,
-                  onPress: () {
-                    // checkValidityUser("Super_User", "Super@123456")
-                    checkValidityUser(usernameTextController.text,
-                            passwordTextController.text)
-                        .then((value) {
-                      if (value == true) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()));
-                      } else {
-                        toastMessagePopup(context, "Invalid Credentials");
-                      }
-                    });
-                  },
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  toastMessagePopup(context,
-                      "This feature will be available in the future update");
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "New to Smartcomm? Sign up Now",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      decoration: TextDecoration.underline,
-                    ),
+                    // child: myText(
+                    //   text: "New to Smartcomm? Sign up Now",
+                    //   fontSize: 18,
+                    //   color: Colors.white,
+                    // ),
                   ),
-                  // child: myText(
-                  //   text: "New to Smartcomm? Sign up Now",
-                  //   fontSize: 18,
-                  //   color: Colors.white,
-                  // ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        // const SizedBox(
-        //   height: 125,
-        // ),
-        const Spacer(),
-        Image.asset(
-          "assets/logo/EAlogowhite.png",
-          height: 40,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-      ],
+          // const SizedBox(
+          //   height: 125,
+          // ),
+          const Spacer(),
+          Image.asset(
+            "assets/logo/EAlogowhite.png",
+            height: 40,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
     );
   }
 }
@@ -203,6 +219,8 @@ Future checkValidityUser(String username, String password) async {
   printBlue("auth not success");
   return false;
 }
+
+bool isPasswordHidden = true;
 
 class LoginCreds {
   final int status;
@@ -290,3 +308,5 @@ class input_cards extends StatelessWidget {
     );
   }
 }
+
+bool ischecking_req = false;
